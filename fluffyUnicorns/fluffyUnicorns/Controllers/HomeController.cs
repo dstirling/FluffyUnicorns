ab12798ade11d2b2aa9bf955449eaa7cd686029d
dstirling <dstirlin@alumni.uoguelph.ca>
2014-05-07 19:11:03 -0400

    Flava Flav text (home page)

--- fluffyUnicorns/fluffyUnicorns/Controllers/HomeController.cs
+++ fluffyUnicorns/fluffyUnicorns/Controllers/HomeController.cs
@@ -10,21 +10,21 @@ namespace fluffyUnicorns.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Fluffy unicorns prancing around, fluffy unicorns once lost, now found.  Can unicorns fly? Apparently only Pegasus have wings.. but what if a pegasus and unicorn did it and had a baby... to make a flying unipeg? pegacorn? Uniasus?  or all three?  ";
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Fluffy Unicorns bein all pretty, looking for a Pegasus so they can fly!";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact the Fluffy Unicorns!";
 
             return View();
         }
