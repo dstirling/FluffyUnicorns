814f274f595cb6e1d48eb33798b4ff6046b77394
dstirling <dstirlin@alumni.uoguelph.ca>
2014-05-13 08:56:32 -0400

    Re-connected- LINQ style

---
+++ fluffyUnicorns/fluffyUnicorns/Controllers/bookmarkController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fluffyUnicorns.Controllers
+{
+    public class bookmarkController : Controller
+    {
+        //
+        // GET: /bookmark/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
